9aa31a6eadeadd2c9eb045700ca94875303c0385
vishnu <vishnurad@outlook.com>
2020-05-09 19:22:39 +1200

    creating ISR and setup loop

--- Flicker_fusion_threshold/src/main.cpp
+++ Flicker_fusion_threshold/src/main.cpp
@@ -10,11 +10,41 @@ The requirements of the project are:
  No delay() function to be used and
  Debouncing of the button to be done*/
 #include <Arduino.h>
+#define PotPin  A8; // input pin for the potentiometer is A8
+#define ledPin  8; //  green LED is pin 8 on teensy
+#define buttonPin 12; // the push button is on pin 12 on teensy
+float Timeperiod = 0; // Timeperiod is read as input from the potentiometer.
 
-void setup() {
-  
+volatile float frequency; //Blink frequency of green LED
+bool ledState = LOW; // ledState used to set the LED
+unsigned long previousMillis = 0; // will store last time LED was updated
+volatile long lastDebounceTime = 0; // output pin previous toggle recorded
+volatile long debounceDelay = 50; // debounce delay time
+//Its given to filter out unwanted switch bounces.
+float time; // to store the value of 'Timeperiod' 
+//which is mapped to a lower range
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT); // this is set as the output pin(8)
+pinMode( buttonPin, INPUT); // this is set as the input pin(12)
+attachInterrupt(digitalPinToInterrupt(12), ISR, FALLING);
+//an interrupt ISR is created
 }
 
 void loop() {
 
+}
+void ISR() // Created Interrupt Service Routine (ISR)
+{
+bool flag;
+lastDebounceTime = micros();
+if ((micros() - lastDebounceTime) > debounceDelay) //debounce
+{
+flag = 1; //to start debounce check
+}
+if (flag ==1 ) // debounce checking
+{
+Serial.print("Current Flicker Freqency = ");
+Serial.println(frequency); // print current frequency of green LED
 }

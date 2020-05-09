63dcf29238f2a2cac08619635ea9a86e7c016cb6
vishnu <vishnurad@outlook.com>
2020-05-09 14:15:47 +1200

    Project_requirements

---
+++ Flicker_fusion_threshold/src/main.cpp
@@ -0,0 +1,20 @@
+/* Project to measure flicker fusion threshold using
+ teensy 3.2 microcontroller.
+The requirements of the project are:
+ A flashing LED should serve as the visual stimulus.
+ A potentiometer should be used to adjust the LED’s blink
+ frequency.
+ A button should be connected which when pressed a hardware
+ interrupt is to be triggered and the currently set blink
+ frequency should be displayed.
+ No delay() function to be used and
+ Debouncing of the button to be done*/
+#include <Arduino.h>
+
+void setup() {
+  
+}
+
+void loop() {
+
+}
